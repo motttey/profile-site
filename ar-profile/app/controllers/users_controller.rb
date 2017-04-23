35f5236a38dfec71e604aa915837782416e56f95
motttey <motitago@gmail.com>
2017-04-24 00:31:34 +0900

    add update

--- ar-profile/app/controllers/users_controller.rb
+++ ar-profile/app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
     def show
 		@user = UserDetail.find_by(:username => params[:username])
     end
+
+    def edit
+		@user = UserDetail.find_by(:username => params[:username])
+    end
+
+    def update
+    	@user = UserDetail.find_by(:username => params[:username])
+
+    	if @user.update_attributes(user_params)
+    		# 成功時の処理
+    	   @user.save
+    	else
+      		render 'edit'
+    	end
+    end
+  private
+
+    def user_params
+      params.permit(:name, :nickname, :about)
+    end
 end
--- ar-profile/config/routes.rb
+++ ar-profile/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
   get 'users/index'
   get 'users/show'
   get 'users/show/:username' => 'users#show'
-
+  get 'users/edit/:username' => 'users#edit'
+  post 'users/edit/:username' => 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
