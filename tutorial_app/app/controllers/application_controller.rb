ccb8e79623cab5063c0e31354200b4a80d68ee58
motttey <motitago@gmail.com>
2017-05-08 14:39:17 +0900

    finish Section #3

--- tutorial_app/app/controllers/application_controller.rb
+++ tutorial_app/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # application#helloで呼び出し
+  def hello
+    render html: "hello, world!"
+  end
 end
